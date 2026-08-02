799b98089c00480bf987fc67a855e757ab72aa44
Sudipta Kumar Nayak <sudipta18nayak@gmail.com>
2026-08-03 01:02:11 +0530

    Add solution: Palindrome Number (Easy)

---
+++ Easy/9_Palindrome_Number.cpp
@@ -0,0 +1,58 @@
+/*
+ * Problem #9: Palindrome Number
+ * Difficulty: Easy
+ *
+ * ----- Description -----
+ *
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ * 
+ *  
+ * Example 1:
+ * 
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * 
+ * 
+ *  
+ * Constraints:
+ * 
+ * 
+ * 	-231 <= x <= 231 - 1
+ * 
+ * 
+ *  
+ * Follow up: Could you solve it without converting the integer to a string?
+ *
+ * ----- Solution -----
+ */
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        return false;
+        int y=x;
+        long long palindrom=0;
+        while(y!=0){
+            int digit= y%10;
+            palindrom= palindrom*10+digit;
+            y/=10;
+        }
+        return(palindrom==x);
+    }
+};
